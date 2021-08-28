a1288acb7dab8a52630cedc7728c6f26eaa9237d
FazleRabbiRana <ranafazrabbi@gmail.com>
2021-08-28 22:03:35 +0600

    country page update

--- js/countries-practice.js
+++ js/countries-practice.js
@@ -16,10 +16,20 @@ const displayCountries = countries => {
 	countries.forEach(country => {
 		const singleCountry = document.createElement('div');
 		singleCountry.classList.add('country');
+		let capital = `${country.capital}`;
+		if(capital == '') {
+			capital = `<small class="text-danger fst-italic">Data not available</small>`;
+		}
 		singleCountry.innerHTML = `
-			<h3>${country.name}</h3>
-			<p><b>Capital:</b> ${country.capital}</p>
-			<button onclick="loadCountryByName('${country.name}')" class="btn btn-success w-100">Details</button>
+			<div class="h-100 d-flex flex-column">
+				<div class="flex-fill">
+					<h3>${country.name}</h3>
+					<p><b>Capital:</b> ${capital}</p>
+				</div>
+				<div class="">
+					<button onclick="loadCountryByName('${country.name}')" class="btn btn-success w-100">Details</button>
+				</div>
+			</div>
 		`;
 		countriesContainer.appendChild(singleCountry);
 	});
@@ -33,13 +43,18 @@ const loadCountryByName = name => {
 }
 
 const showCountryDetails = country => {
+	console.log(country);
 	const countryDetails = document.getElementById('country-detail');
 	const languages = country.languages.map(language => language.name);
 	const currencies = country.currencies.map(currency => currency.name);
+	let capital = `${country.capital}`;
+	if(capital == '') {
+		capital = `<small class="text-danger fst-italic">Data not available</small>`;
+	}
 	countryDetails.innerHTML = `
 		<h4>${country.name}</h4>
 		<img src="${country.flag}" alt="${country.name}" class="mt-1 mb-2">
-		<p><b>Capital:</b> ${country.capital}</p>
+		<p><b>Capital:</b> ${capital}</p>
 		<p><b>Language:</b> ${languages.join(', ')}</p>
 		<p><b>Currency:</b> ${currencies.join(', ')}</p>
 	`;
